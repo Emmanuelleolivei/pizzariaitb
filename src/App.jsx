7890bfe913fbf879b0c9a4798998eea1ea2e027a
emmanuelleolivei <emmauelleoliveira878@gmail.com>
2024-07-29 12:25:59 -0300

    tudo acabará em pizza

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import {useState} from 'react'
 import './App.css'
-import { Routes, route } from 'react-router-dom'
+import {Routes, route } from 'react-router-dom'
+import Listagem from './pages/listagem';
 
-const App = () => {
+const App = () =>  {
+
+  const [color] = useState("#0000ff")
+  document.body.style.backgroundcolor = color
+
   return (
-    <Routes>
-      <Routes path="/" />
-    </Routes>
-  )  
-} 
+  <Routes>
+    <Route path="/" element={<listagem />}/>
+  </Routes>
+  )
+}
 
 export default App;
-   
